4458e558a8b6d5feaaebc7f0206351a0f603b9c4
treephesians <136706252+treephesians@users.noreply.github.com>
2023-06-28 17:29:25 +0900

    modify img_crawling_google.py

--- img_crawling_google.py
+++ img_crawling_google.py
@@ -8,12 +8,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 ssl._create_default_https_context = ssl._create_unverified_context
 
-searchKey = input('검색 키워드 입력:')
+searchKey = input('Search Keyword : ')
+
+# 절대경로로 저장할 시, txt 파일에 주소를 저장 먼저 하기. (주소를 숨기기 위해서)
+filename = "path.txt"
+if os.path.exists(filename):
+    f = open(filename, 'r')
+    line = f.read()
+    path = f"{line}/{searchKey}/images"
+else:
+    # 상대경로
+    print("Ther is no path file. Save in this project.")
+    path = f"./imgs/{searchKey}/images"
+
 
 try:
     # 중복되는 폴더 명이 없다면 생성
-    if not os.path.exists(f"./imgs/{searchKey}"):
-        os.makedirs(f"./imgs/{searchKey}")
+    if not os.path.exists(path):
+        os.makedirs(path)
     # 중복된다면 문구 출력 후 프로그램 종료
     else:
         print('이전에 같은 [검색어, 이미지 수]로 다운로드한 폴더가 존재합니다.')
@@ -50,7 +62,7 @@ while True:
             break
     last_height = new_height
 
-#이건 구글 작은 사진들 모으는거고 클래쓰 두개 뛰어쓰면 안된다!
+# 이건 구글 작은 사진들 모으는거고 클래쓰 두개 뛰어쓰면 안된다!
 images = driver.find_elements(By.CSS_SELECTOR, ".rg_i.Q4LuWd")
 print(images)
 
@@ -60,8 +72,7 @@ for image in images:
         print(count)
         image.click()
         time.sleep(0.5)
-        #xpath = r'//*[@id="Sva75c"]/div[2]/div/div[2]/div[2]/div[2]/c-wiz/div/div/div/div[3]/div[1]/a/img[1]'
-        #xpath = r'//*[@id="Sva75c"]/div[2]/div[2]/div[2]/div[2]/c-wiz/div/div/div/div[3]/div[1]/a/img[1]'
+        # 크롤링이 안될시 xpath를 한번 수정해봐라
         xpath = r'//*[@id="Sva75c"]/div[2]/div[2]/div[2]/div[2]/c-wiz/div/div/div/div[3]/div[1]/a/img[1]'
         imgUrl = driver.find_element(
             By.XPATH,
@@ -73,10 +84,10 @@ for image in images:
              'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')
         ]
         urllib.request.install_opener(opener)
-        if searchKey == "눈사람":
-            urllib.request.urlretrieve(imgUrl, f'./imgs/{searchKey}/snowman{str(count)}.jpg')
+        if searchKey == "눈사람": # snowman 노래 및 연예인도 있어서 한글로 검색 후 영어로 저장.
+            urllib.request.urlretrieve(imgUrl, f'{path}/{searchKey}/snowman{str(count)}.jpg')
         else:
-            urllib.request.urlretrieve(imgUrl, f'./imgs/{searchKey}/{searchKey}{str(count)}.jpg')
+            urllib.request.urlretrieve(imgUrl, f'{path}/{searchKey}{str(count)}.jpg')
         count = count + 1
         if count > 500:
             break
